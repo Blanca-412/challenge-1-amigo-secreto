5d81b197244f2517f8fe915eff5db7efb39363a6
Blanca-412 <blancaarisg@gmail.com>
2025-03-18 11:14:21 -0600

    Se implementó función para agregar amigos

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
+//let amigoSecreto = "";
 
+function agregarAmigo() {
+    let nuevoAmigo = document.getElementById("amigo").value;
+    console.log (nuevoAmigo);
+    console.log (amigos);
+        if (nuevoAmigo == "") {
+        alert("Por favor, inserte un nombre.");
+    } else {
+        amigos.push(nuevoAmigo);
+        limpiarCampo();
+        return nuevoAmigo;
+    }
+}
+
+function limpiarCampo() {
+    document.getElementById("amigo").value = "";
+}
